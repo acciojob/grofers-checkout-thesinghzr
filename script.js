9c1793d28a14da54e47068d3a1115727bd22c2cb
Suraj Singh <yousurajsingh@gmail.com>
2025-03-27 22:17:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,27 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+const priceelem=document.querySelectorAll(".price")	;
+	let totprice=0;
+
+	priceelem.forEach(element => {
+		const price=parseFloat(element.textContent);
+		if(!isNaN(price))
+		{
+			totprice += price;
+		}
+	});
+
+	const table=document.querySelector("table");
+
+	const newrow=document.createElement("tr");
+	const totcell=document.createElement("td");
+	totcell.id="ans";
+
+	totcell.textContent=`Total Price:${totprice.toFixed(2)}`;
+	totcell.colSpan=table.rows[0].cells.length;
+	newrow.appendChild(totcell);
+  table.appendChild(newrow);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
